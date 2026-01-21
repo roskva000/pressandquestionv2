af91dd2138e5b51e96813f0eb616d8befa80312d
roskva000 <suzenfurkan531@gmail.com>
2026-01-21 17:00:47 +0300

    Update SW to Network-First strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'press-question-v1';
+const CACHE_NAME = 'press-question-v2';
 const ASSETS = [
     '/',
     '/index.html',
@@ -19,8 +19,35 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        fetch(event.request)
+            .then((response) => {
+                // Network request succeeded
+                // Clone the response because it can only be consumed once
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            })
+            .catch(() => {
+                // Network request failed, try cache
+                return caches.match(event.request);
+            })
+    );
+});
+
+// Clean up old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
